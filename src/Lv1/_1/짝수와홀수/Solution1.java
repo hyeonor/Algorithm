e50a46f8d869936f2f61e5c8297580d7ea5f9491
hyeonorof <hyeonorof@gmail.com>
2022-10-16 18:50:46 +0900

    짝수와 홀수

---
+++ src/Lv1/_1/짝수와홀수/Solution1.java
@@ -0,0 +1,15 @@
+package Lv1._1.짝수와홀수;
+
+public class Solution1 {
+
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        Solution1 s = new Solution1();
+
+        System.out.println(s.solution(3));
+        System.out.println(s.solution(4));
+    }
+}
