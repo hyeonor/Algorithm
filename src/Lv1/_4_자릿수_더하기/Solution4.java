a5e5b9018ea03fd95df8b8794b38882e81abfe64
hyeonorof <hyeonorof@gmail.com>
2022-10-19 11:20:41 +0900

    자릿수 더하기

---
+++ src/Lv1/_4_자릿수_더하기/Solution4.java
@@ -0,0 +1,21 @@
+package Lv1._4_자릿수_더하기;
+
+public class Solution4 {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n = n / 10;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution4 s = new Solution4();
+
+        System.out.println(s.solution(123));
+        System.out.println(s.solution(987));
+    }
+}
