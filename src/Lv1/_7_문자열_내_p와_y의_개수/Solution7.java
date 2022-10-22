78f23f81fec07e7faa8c536c5cc2a0db04d1a18b
hyeonorof <hyeonorof@gmail.com>
2022-10-22 19:58:28 +0900

    문자열 내 p와 y의 개수

---
+++ src/Lv1/_7_문자열_내_p와_y의_개수/Solution7.java
@@ -0,0 +1,28 @@
+package Lv1._7_문자열_내_p와_y의_개수;
+
+public class Solution7 {
+
+    boolean solution(String s) {
+        int pcnt = 0, ycnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            String sub = s.substring(i, i + 1);
+            if (sub.equals("p") || sub.equals("P"))
+                pcnt++;
+            else if (sub.equals("y") || sub.equals("Y"))
+                ycnt++;
+        }
+
+        return pcnt == ycnt;
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+
+        String str = "pPoooyY";
+        String str1 = "Pyy";
+
+        System.out.println(s.solution(str));
+        System.out.println(s.solution(str1));
+    }
+}
