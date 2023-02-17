ae2eead56d55ce0b684f2727fcf69bce4950c739
hyeonorof <hyeonorof@gmail.com>
2023-02-17 23:46:21 +0900

    좌표_정렬하기

---
+++ src/BaekJoon/정렬/좌표_정렬하기/Main.java
@@ -0,0 +1,39 @@
+package BaekJoon.정렬.좌표_정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void sort(int[][] arr) {// 11650번
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int[] a : arr) {
+            System.out.println(a[0] + " " + a[1]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int size = Integer.parseInt(br.readLine());
+        int[][] arr = new int[size][2];
+
+        for (int i = 0; i < size; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        sort(arr);
+    }
+}
