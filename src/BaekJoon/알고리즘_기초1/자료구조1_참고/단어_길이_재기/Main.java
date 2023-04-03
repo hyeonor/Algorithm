ed47d4776ec38c77ee44ee62841a44a0ecb539fb
hyeonorof <hyeonorof@gmail.com>
2023-04-03 20:20:25 +0900

    단어 길이 재기

---
+++ src/BaekJoon/알고리즘_기초1/자료구조1_참고/단어_길이_재기/Main.java
@@ -0,0 +1,15 @@
+package BaekJoon.알고리즘_기초1.자료구조1_참고.단어_길이_재기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s.length());
+    }
+}
