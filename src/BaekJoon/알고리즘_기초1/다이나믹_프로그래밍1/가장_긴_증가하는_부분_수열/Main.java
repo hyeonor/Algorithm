ec9e72c0a3d7dd7fc9b41033749783297f92f0a8
hyeonorof <hyeonorof@gmail.com>
2023-04-13 21:42:04 +0900

    가장 긴 증가하는 부분 수열

---
+++ src/BaekJoon/알고리즘_기초1/다이나믹_프로그래밍1/가장_긴_증가하는_부분_수열/Main.java
@@ -0,0 +1,39 @@
+package BaekJoon.알고리즘_기초1.다이나믹_프로그래밍1.가장_긴_증가하는_부분_수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {//  11053번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        LIS(n, arr, dp);
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+
+    }
+
+    private static void LIS(int n, int[] arr, int[] dp) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+    }
+}
