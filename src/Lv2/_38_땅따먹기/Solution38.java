3050b7a8c5177436730d0b35c8117106c77c3ef0
hyeonorof <hyeonorof@gmail.com>
2023-04-28 21:27:49 +0900

    땅따먹기 수정

--- src/Lv2/_38_땅따먹기/Solution38.java
+++ src/Lv2/_38_땅따먹기/Solution38.java
@@ -3,7 +3,7 @@ package Lv2._38_땅따먹기;
 public class Solution38 {
 
     int solution(int[][] land) {
-        int answer = 0;
+        int n = land.length - 1;
 
         for (int i = 1; i < land.length; i++) {
             land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
@@ -12,11 +12,7 @@ public class Solution38 {
             land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
         }
 
-        for (int i = 0; i < land.length; i++) {
-            answer = Math.max(answer, land[land.length - 1][i]);
-        }
-
-        return answer;
+        return Math.max(land[n][0], Math.max(land[n][1], Math.max(land[n][2], land[n][3])));
     }
 
     public static void main(String[] args) {
