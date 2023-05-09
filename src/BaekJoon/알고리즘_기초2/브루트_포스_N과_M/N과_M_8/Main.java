7c673bfbbeb19686578210ddac27ae7e6c913417
hyeonorof <hyeonorof@gmail.com>
2023-05-09 17:50:50 +0900

    N과 M (8)

---
+++ src/BaekJoon/알고리즘_기초2/브루트_포스_N과_M/N과_M_8/Main.java
@@ -0,0 +1,51 @@
+package BaekJoon.알고리즘_기초2.브루트_포스_N과_M.N과_M_8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {// 15657번
+    static int N, M;
+    static int[] ans;
+    static int[] arr;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int idx, int depth) {
+        if (depth == M) {
+            for (int a : ans) {
+                sb.append(a).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            ans[depth] = arr[i];
+            dfs(i, depth + 1);
+        }
+    }
+}
